ff70910c302f48a72a2faa81e2300ad452e0b19b
Martin Julian Osorio Marulanda <martin.osorio@MARTINOSORIO.pragma.com.co>
2020-02-27 21:39:02 -0500

    Agregar archivos de proyecto.

---
+++ WebApiM3/Context/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApiM3.Entities;
+
+namespace WebApiM3.Context
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+               : base(options)
+        {
+
+        }
+
+        public DbSet<Autor> Autores { get; set; }
+    }
+}
---
+++ WebApiM3/Entities/Autor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiM3.Entities
+{
+    public class Autor
+    {
+        public int Id { get; set; }
+        public String Nombre { get; set; }
+    }
+}
